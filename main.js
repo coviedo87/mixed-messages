cf55ba4ce8339dd46afd4b85312f93c0f5ad24e5
coviedo <carlos27135@gmail.com>
2025-02-19 15:45:57 -0500

    main files

---
+++ main.js
@@ -0,0 +1,18 @@
+/* 
+-------------------------------
+Project: Mixed Messages
+Author: Carlos Oviedo
+version: 1
+Date: 02/19/2025
+
+Description: random message generator
+------------------------------
+*/
+
+//array with inspirational quoates
+
+//function randomQuotes
+    //get a random number from 0 - 9
+    // save random number
+    //use it to display one of the arr elements
+    
