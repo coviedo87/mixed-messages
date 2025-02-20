f4e1064149b0fc161582d23cb954523cabaf41fb
coviedo <carlos27135@gmail.com>
2025-02-20 17:31:54 -0500

    new random message generator

---
+++ script.js
@@ -0,0 +1,25 @@
+/* 
+-------------------------------
+Project: Mixed Messages
+Author: Carlos Oviedo
+version: 2
+Date: 02/20/2025
+
+Description: random message generator
+------------------------------
+*/
+
+//arr to store dates(today, tomorrow, next week)
+//arr to store mementos
+//arr to store emojies
+
+
+//function generate random memento
+    //store 4 random numbers 
+    // take 3 random numbers to create message + log the last random number as lucky number
+
+
+//call the function
+
+//next steps, use the index.html to display the message in the web browser bonus: style it
+
