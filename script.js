7f810838cffb84bddb5aa0220a47d4d79736741d
coviedo <carlos27135@gmail.com>
2025-02-25 17:38:36 -0500

    final revision

--- script.js
+++ script.js
@@ -9,17 +9,33 @@ Description: random message generator
 ------------------------------
 */
 
-//arr to store dates(today, tomorrow, next week)
-//arr to store mementos
-//arr to store emojies
+ const times = ["Time to", "Today", "This week", "Remember to"];
+ const emojies = ["🤙🏼", "🙌", "🤝", "👌", "🤗", "💫", "☘️", "💡", "🗝️", "💚"];
+ const mementos = [
+    "try again",
+    "pick a good one",
+    "stick together",
+    "pick up the pace",
+    "make some noice"
+ ]
 
+//Generates random memento message
+function randomMessage () {
+    //store index for times arr
+    let num1 = Math.floor(Math.random() * (times.length - 0) + 0);
 
-//function generate random memento
-    //store 4 random numbers 
-    // take 3 random numbers to create message + log the last random number as lucky number
+    //store index for emojies arr
+    let num2 = Math.floor(Math.random() * (emojies.length - 0) + 0);
+
+    //store index for mementos arr
+    let num3 = Math.floor(Math.random() * (mementos.length - 0) + 0);
+    
+    console.log(`${times[num1]} ${mementos[num3]}! ${emojies[num2]}`);
+}
 
 
 //call the function
+randomMessage();
 
 //next steps, use the index.html to display the message in the web browser bonus: style it
 
